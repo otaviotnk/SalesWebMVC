938599a13a2337ce47f39cb990b38498906008bc
Otavio Rockenbach <otavio.atr@gmail.com>
2020-04-20 15:20:25 -0300

    Other Entities and Second Migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,9 +7,28 @@ using System.Threading.Tasks;
 namespace SalesWebMVC.Models
 {
     public class Department
-    {
-
-        public int Id { get; set; }
+    {        public int Id { get; set; }
         public string Name { get; set; }
+
+        //implementando a associacao ao seller
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department()
+        {
+
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }

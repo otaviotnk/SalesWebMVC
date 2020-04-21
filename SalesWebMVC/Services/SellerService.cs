05ac42d8693638701546ad6e567ce27e1122ead8
Otavio Rockenbach <otavio.atr@gmail.com>
2020-04-21 10:35:23 -0300

    Seller Details and Eager Loading

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
 using SalesWebMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace SalesWebMVC.Services
@@ -31,8 +32,8 @@ namespace SalesWebMVC.Services
         }
 
         public Seller FindById(int id)
-        {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        {            
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)

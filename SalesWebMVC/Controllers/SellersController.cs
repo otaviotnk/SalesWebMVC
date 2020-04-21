b399a7e6a450c32617f700910cfcd8ddca1388c5
Otavio Rockenbach <otavio.atr@gmail.com>
2020-04-20 21:22:29 -0300

    Simple Create Form

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Services;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
 {
@@ -22,6 +23,20 @@ namespace SalesWebMVC.Controllers
             //retorna uma lista de Seller
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Anotation indicando que é post e nao get
+        [HttpPost]
+        //Evitar ataque CSRF
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
 
     }

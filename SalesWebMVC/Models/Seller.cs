fbe815df8fb26de669879fbbb0a16dc9c475bc6f
Otavio Rockenbach <otavio.atr@gmail.com>
2020-04-22 20:00:21 -0300

    Update CultureInfo and Sellers

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -2,50 +2,43 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
-
 
 namespace SalesWebMVC.Models
 {
     public class Seller
     {
-        [Required(ErrorMessage = "{0} required")]
         public int Id { get; set; }
 
-
         [Required(ErrorMessage = "{0} required")]
-        [StringLength(60, MinimumLength = 5, ErrorMessageResourceName = "{0} size should beteween {2} and {1}")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
         [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Display(Name = "Birth Date")]
 
         [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Range(100.00, 50000.0, ErrorMessage = "{0} must be from {1} and {2}")]
         [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         public double BaseSalary { get; set; }
 
-
         public Department Department { get; set; }
-
-
         public int DepartmentId { get; set; }
 
-
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
         public Seller()
         {
-
         }
+
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
@@ -60,6 +53,7 @@ namespace SalesWebMVC.Models
         {
             Sales.Add(sr);
         }
+
         public void RemoveSales(SalesRecord sr)
         {
             Sales.Remove(sr);
